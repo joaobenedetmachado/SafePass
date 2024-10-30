ed671a0d6f9b439db801a6c175a1fe434f2df5de
joao victor benedet machado <jvbm2177@gmail.com>
2024-10-30 20:05:47 -0300

    agora ele recarrega apos criar a senha

--- main.py
+++ main.py
@@ -6,15 +6,17 @@ import encrypt as enc
 # Configurações do banco de dados
 DB_HOST = "127.0.0.1"
 DB_USER = "root"
-DB_PASSWORD = ""
+DB_PASSWORD = "123123123"
 DB_DATABASE = "vaultify"
+DB_PORT = 3307 # deletar essa linha 
 
 # Conexão com o banco de dados
 DBconexao = mysql.connector.connect(
     host=DB_HOST,
     user=DB_USER,
     password=DB_PASSWORD,
-    database=DB_DATABASE
+    database=DB_DATABASE,
+    port = DB_PORT # deletar esse linha e a virgula de cima
 )
 
 cursor = DBconexao.cursor()
@@ -22,8 +24,9 @@ cursor = DBconexao.cursor()
 # Criação da janela principal
 root = tk.Tk()
 root.title("Cadastro e Login")
-root.geometry("400x400")
+root.geometry("600x400")
 root.configure(bg="#f0f0f0")  # Cor de fundo
+root.resizable(False, False)
 
 # Estilização do notebook
 style = ttk.Style()
@@ -34,7 +37,7 @@ notebook = ttk.Notebook(root)
 notebook.pack(pady=10, expand=True, fill='both', padx=20)  # Adicionado fill='both' e padx
 
 # Criando as abas com tamanho específico
-aba_login = ttk.Frame(notebook, width=360, height=360)  # Define tamanho específico
+aba_login = ttk.Frame(notebook, width=400, height=360)  # Define tamanho específico
 aba_login.pack(fill='both', expand=True)  # Faz a aba expandir para preencher o espaço
 
 aba_cadastro = ttk.Frame(notebook, width=360, height=360)  # Define tamanho específico
@@ -80,7 +83,7 @@ def logar():
     else:
         messagebox.showwarning("Login", "Por favor, preencha todos os campos.")
 
-# Função após login
+# funcao apos o login
 def afterLogin(usuario):
     notebook.forget(aba_cadastro)
     notebook.forget(aba_login)
@@ -96,26 +99,38 @@ def afterLogin(usuario):
     comandoSQL = f"SELECT idusers FROM users WHERE nome = '{usuario}'"
     cursor.execute(comandoSQL)
     resultado = cursor.fetchone()
-
+    iduser = resultado[0]
     if resultado:
-        iduser = resultado[0]
-        comandoSQL = f"SELECT passwordhash FROM passwords WHERE userid = {iduser}"
-        cursor.execute(comandoSQL)
-        resultadoSenhas = cursor.fetchall()
-        text_area.delete(1.0, tk.END)
-        if resultadoSenhas: 
-            for senha in resultadoSenhas:
-                senhaEncriptografada = senha[0]
-                text_area.insert(tk.END, f"{enc.descriptografar(senhaEncriptografada)}\n")
-        else:
-            text_area.insert(tk.END, "Nenhuma senha encontrada.")
-
+        recarregar(resultado, text_area)
     text_area.config(state='disabled')
-    button_criarNovaSenha = tk.Button(aba_senhas, text="Cadastrar Nova Senha", command=lambda: cadastrarNovaSenhaArea(iduser))
+    button_criarNovaSenha = tk.Button(aba_senhas, text="Cadastrar Nova Senha", command=lambda: cadastrarNovaSenhaArea(iduser, resultado, text_area))
     button_criarNovaSenha.pack(pady=(20, 20))
 
-# Função para cadastrar nova senha
-def cadastrarNovaSenhaArea(iduser):
+def recarregar(resultado, text_area):
+    text_area.config(state='normal')
+    print(resultado, text_area)
+    iduser = resultado[0]
+    comandoSQL = f"SELECT passwordhash, sitename FROM passwords WHERE userid = {iduser}"
+    cursor.execute(comandoSQL)
+    resultadoSenhas = cursor.fetchall()
+
+    text_area.delete(1.0, tk.END)
+    text_area.insert(tk.END, f"{'Senha'.ljust(24)} | {'Site'}\n")
+    text_area.insert(tk.END, "-" * 40 + "\n")
+
+    if resultadoSenhas:
+        for senha, siteName in resultadoSenhas:
+            senhaDescriptografada = enc.descriptografar(senha)
+            text_area.insert(tk.END, f"{senhaDescriptografada.ljust(24)} | {siteName}\n")
+    else:
+        text_area.insert(tk.END, "Nenhuma senha encontrada.")
+        text_area.config(state='disable')
+
+
+
+
+# func p cadastrar senha nova no db
+def cadastrarNovaSenhaArea(iduser, resultado, text_area):
     janelaCriarSenha = tk.Toplevel(root)
     janelaCriarSenha.title("Criar Senha")
     janelaCriarSenha.geometry("300x300")
@@ -129,17 +144,18 @@ def cadastrarNovaSenhaArea(iduser):
     label_sitename.pack(pady=(20, 5))  
     entry_sitename = tk.Entry(janelaCriarSenha)
     entry_sitename.pack(pady=(0, 20))  
-    button_criarNovaSenha = tk.Button(janelaCriarSenha, text="Cadastrar Nova Senha", command=lambda: CadastrarNovaSenha(janelaCriarSenha, iduser, enc.criptografar(entry_senha.get()), entry_sitename.get()))
+    button_criarNovaSenha = tk.Button(janelaCriarSenha, text="Cadastrar Nova Senha", command=lambda: CadastrarNovaSenha(janelaCriarSenha, iduser, enc.criptografar(entry_senha.get()), entry_sitename.get(), resultado, text_area))
     button_criarNovaSenha.pack(pady=(20, 20))
 
-def CadastrarNovaSenha(janelaCriarSenha, iduser, password, siteName):
+def CadastrarNovaSenha(janelaCriarSenha, iduser, password, siteName, resultado, text_area):
     comandoSQL = f'INSERT INTO passwords(userid, passwordhash, sitename) VALUES({iduser}, "{password}", "{siteName}")'
     cursor.execute(comandoSQL)
     DBconexao.commit()
     print("Senha cadastrada")
+    recarregar(resultado, text_area)
     janelaCriarSenha.destroy()
 
-# Área de cadastro
+# cadastro
 label_usuario_cadastro = tk.Label(aba_cadastro, text="Usuário:", bg="#f0f0f0")
 label_usuario_cadastro.pack(pady=(20, 5))  
 entry_usuario_cadastro = tk.Entry(aba_cadastro)
@@ -152,7 +168,7 @@ entry_senha_cadastro.pack(pady=(0, 20))
 button_cadastrar = tk.Button(aba_cadastro, text="Cadastrar", command=cadastrar)
 button_cadastrar.pack(pady=(20, 20))  
 
-# Área de login
+#login
 label_usuario_login = tk.Label(aba_login, text="Usuário:", bg="#f0f0f0")
 label_usuario_login.pack(pady=(20, 5))  
 entry_usuario_login = tk.Entry(aba_login)
@@ -166,5 +182,4 @@ entry_senha_login.pack(pady=(0, 20))
 button_logar = tk.Button(aba_login, text="Login", command=logar)
 button_logar.pack(pady=(20, 20)) 
 
-# Iniciar o loop principal
 root.mainloop()
