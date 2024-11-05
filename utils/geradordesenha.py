9fa9c9c7306498b1fc9b1fa63d85dd286c95cb98
GustavoDeBemLino <gugu11debem2007@gmail.com>
2024-11-05 16:33:11 -0300

    Update geradordesenha.py
    Código comentado

--- utils/geradordesenha.py
+++ utils/geradordesenha.py
@@ -1,12 +1,13 @@
+#Esta linha importa bibliotecas para o python
 import numpy as np
 import random
 
-
+#Estas 3 linhas colocam os números, caracteres especiais e letras em váriaveis
 ascii_letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
 digits = '1234567890'
 punctuation = '_-.'
 
-
+#Esta função pega os números/caracteres especiais/letras e randomifica de acordo com a quantidade desejada pelo cliente.
 def GerarSenhas(numero):
   letras = list(ascii_letters)
   numeros = list(digits)
