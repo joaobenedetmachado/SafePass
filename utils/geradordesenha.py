fb432ba43389a27fd4d54a706dce7c978b5852c3
GustavoDeBemLino <gugu11debem2007@gmail.com>
2024-11-05 14:15:08 -0300

    gerador de senhas feito

--- utils/geradordesenha.py
+++ utils/geradordesenha.py
@@ -1,11 +1,32 @@
+import numpy as np
 import random
 
-caracteres = list('QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklçzxcvbnm@#%-*')
 
-def GerarSenhas(number):
-    senha = []
-    for _ in range(number):
-        letraEsc = caracteres[random.randint(0, len(caracteres) - 1)]
-        senha.append(letraEsc)
-    return ''.join(senha)
+ascii_letters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+digits = '1234567890'
+punctuation = '_-.'
+
+pergunta = int(input("Digite quantos caracteres você quer que a sua senha tenha:"))
+
+if pergunta >=7 and pergunta <= 13:
+    letras = list(ascii_letters)
+    numeros = list(digits)
+    especial = list(punctuation)
+    tds_carac = letras+numeros+especial
+    senha = np.random.choice(list(tds_carac),pergunta)
+    print(''.join(senha))
+
+elif pergunta <7:
+    print("Senha inválida, poucos caractéres na senha")
+
+elif pergunta >13:
+    print("Senha inválida, quantidade desenecessária de caractéres.")
+
+
+# def GerarSenhas(number):
+
+#     return senha
+
+
+# GerarSenha(10)
 
